89764220c6d760c6f6be398c9796756b88c5d2a0
Sai Varun <yadavalli.varun.30.2@mountblue.tech>
2024-11-30 15:03:21 +0530

    Created service layer for User

--- src/main/java/com/stackoverflow/service/UserService.java
+++ src/main/java/com/stackoverflow/service/UserService.java
@@ -1,5 +1,20 @@
 package com.stackoverflow.service;
 
-public interface UserService {
+import com.stackoverflow.model.Answer;
+import com.stackoverflow.model.Notification;
+import com.stackoverflow.model.Question;
+import com.stackoverflow.model.User;
 
+import java.util.List;
+
+public interface UserService {
+    User registerUser(String username, String email, String password);
+    User authenticateUser(String email, String password);
+    User getUserById(Long userId);
+    User getUserByUsername(String username);
+    User updateUserProfile(Long userId, String bio);
+    List<Question> getQuestionsByUser(Long userId);
+    List<Answer> getAnswersByUser(Long userId);
+    List<Notification> getNotificationsByUser(Long userId);
+    void deleteUser(Long userId);
 }
---
+++ src/main/java/com/stackoverflow/service/UserServiceImpl.java
@@ -0,0 +1,87 @@
+package com.stackoverflow.service;
+
+import com.stackoverflow.model.*;
+import com.stackoverflow.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService{
+    private UserRepository userRepository;
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    @Override
+    public User registerUser(String username, String email, String password) {
+        if (userRepository.existsByUsername(username)) {
+            throw new IllegalArgumentException("Username already exists.");
+        }
+        if (userRepository.existsByEmail(email)) {
+            throw new IllegalArgumentException("Email already registered.");
+        }
+        User user = new User();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setProfile(new Profile());
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User authenticateUser(String email, String password) {
+        return null;
+    }
+
+    @Override
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User with ID " + userId + " not found."));
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User with username " + username + " not found."));
+    }
+
+    @Override
+    public User updateUserProfile(Long userId, String bio) {
+        User user = getUserById(userId);
+        Profile profile = user.getProfile();
+        profile.setBio(bio);
+        user.setProfile(profile);
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<Question> getQuestionsByUser(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if(optionalUser.isPresent())
+            return optionalUser.get().getQuestions();
+        throw new RuntimeException("User not found");
+    }
+
+    @Override
+    public List<Answer> getAnswersByUser(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isPresent())
+            return optionalUser.get().getAnswers();
+        throw new RuntimeException("User not found to load answers");
+    }
+
+    @Override
+    public List<Notification> getNotificationsByUser(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isPresent())
+            return optionalUser.get().getNotifications();
+        throw new RuntimeException("User not found to load notifications");
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        userRepository.deleteById(userId);
+    }
+}
