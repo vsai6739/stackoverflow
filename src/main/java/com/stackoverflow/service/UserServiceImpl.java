7afdc19737f60d7f014f2fa659bf8330f16f4718
chandanadinnipati <dinnipatichandana@gmail.com>
2024-12-02 18:09:09 +0530

    chandana

--- src/main/java/com/stackoverflow/service/UserServiceImpl.java
+++ src/main/java/com/stackoverflow/service/UserServiceImpl.java
@@ -89,7 +89,7 @@ public class UserServiceImpl implements UserService{
     public void saveUser(User user) {
         userRepository.save(user);
     }
-        userRepository.save(user);}
+
 
     public User login(String email, String password) {
         if (email == null || email.isEmpty()) {
